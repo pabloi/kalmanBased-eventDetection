6afd53cc57461ca9e6237207b31d241769787649
Unknown <itu.pablo@gmail.com>
2018-09-11 19:23:30 -0400

    Simplified, improved viz

--- kalmanStanceDetector.m
+++ kalmanStanceDetector.m
@@ -1,15 +1,9 @@
-function [pSmoothed,pUpdated,pPredicted,BW]=kalmanStanceDetector(force1,force2)
+function [pSmoothed,pUpdated,pPredicted,pStateGivenPrevious,pObsGivenState]=kalmanStanceDetector(force1,force2)
 %This function implements a kalman-style filter (hidden markov chain state
 %estimation) to infer stance from force traces.
 
-BW=nanmean(force1+force2); %Median has better outlier rejection, but very skewed distributions may make it not a good estimator of weight (newton says mean(force in z direction) = weight because we are not floating away!
-N=length(force1);
 M=100; %Number of points where we estimate the probability distribution in the interval [-1 1]
 D=100; %Number of points where we estimate the probability distribution of the output 
-yObs=[force1 force2]/BW; %We have two observations that roughly will sum to 0, and both components are roughly always in [-1 1]
-%yObs=(force1-force2)/BW;
-%Alt idea: normalize the observations so that they sum to 1 on each point
-%in time (i.e. we only care about force ratio)
 
 %HMM chain description:
 %The transition probability is: with probability=w we transition to a new
@@ -31,24 +25,17 @@ pStateGivenPrevious(M,M)=w+(1-w)*pStateGivenPrevious(M,M);
 measNoiseSigma=.03;
 aux=[0:D-1]'/(D-1) - [0:M-1]/(M-1);
 pObsGivenState=exp(-(aux.^2)/(2*measNoiseSigma^2))/sqrt(2*pi*measNoiseSigma^2);
-pObsGivenState(:,1)= [.9; zeros(D-1,1)] + .1*pObsGivenState(:,1);%If state=1, then with high likelihood we observe 1
-pObsGivenState(:,M)= [zeros(D-1,1);.9] + .1*pObsGivenState(:,M);%If state=1, then with high likelihood we observe 1
+pp=.97;
+pObsGivenState(:,1)= [pp; zeros(D-1,1)] + (1-pp)*pObsGivenState(:,1);%If state=1, then with high likelihood we observe 1
+pObsGivenState(:,M)= [zeros(D-1,1);pp] + (1-pp)*pObsGivenState(:,M);%If state=1, then with high likelihood we observe 1
 
 
 
 
 
 p0=ones(1,M)/M;
-observation=-diff(yObs,[],2)./abs(sum(yObs,2));
-%observation(yObs(1,:)==0)=1; %Assuming not both at 0 at the same time, ever
-%observation(yObs(2,:)==0)=-1;
-observation=round((D-1)*(observation+1)/2)+1; %Quantizing observations to [1,D] range
+observation=discretizeObs((force2-force1)./abs(force2+force1),D,[-1 1]);
 [pPredicted, pUpdated, pSmoothed] = genKFstationaryInference(observation,pObsGivenState,pStateGivenPrevious,p0);
-
-
-figure; subplot(1,2,2); imagesc(pStateGivenPrevious); title('Transition'); ylabel('Next state'); xlabel('Curr state'); subplot(1,2,1); imagesc(pObsGivenState); title('Observation');  ylabel('Obs'); xlabel('State');
-end
-
-function p=normalize(p)
-    p=p/sum(p(:));
+%Visualize matrices:
+%figure; subplot(1,2,2); imagesc(pStateGivenPrevious); title('Transition'); ylabel('Next state'); xlabel('Curr state'); subplot(1,2,1); imagesc(pObsGivenState); title('Observation');  ylabel('Obs'); xlabel('State');
 end
--- mainTest.m
+++ mainTest.m
@@ -9,23 +9,28 @@ Lfz=forces06(:,strcmp(Flabels,'LFz'));
 Rfz=forces06(:,strcmp(Flabels,'RFz'));
 
 %% Do the estimation
-[pS,pU,pP,BW]=kalmanStanceDetector(Lfz,Rfz); %This actually estimates the % Fz force being exerted by the left leg (as opposed to the right)
+[pS,pU,pP,T,O]=kalmanStanceDetector(Lfz,Rfz); %This actually estimates the % Fz force being exerted by the left leg (as opposed to the right)
 [~,MAPstate]=max(pS');
+%% Try EM:
+D=100;
+M=size(pS,2);
+observation=discretizeObs((Rfz-Lfz)./abs(Rfz+Lfz),D,[-1 1]);
+[O,T]=genEM(observation,ones(M,1)/M,O,T);
 %% Compute associated events: LHS, RHS, LTO, RTO
 eventsK=events06;
 eventsK(2:end,1)= MAPstate(2:end)>1 & MAPstate(1:end-1)==1; %LHS
 eventsK(2:end,2)= MAPstate(2:end)<100 & MAPstate(1:end-1)==100; %RHS
 eventsK(2:end,3)= MAPstate(2:end)==1 & MAPstate(1:end-1)>1; %LTO
-eventsK(2:end,4)= MAPstate(2:end)==100 & MAPstate(1:end-1)<100; %LTO
+eventsK(2:end,4)= MAPstate(2:end)==100 & MAPstate(1:end-1)<100; %RTO
 %% Visualize results
-figure; 
-p1=subplot(4,1,1); imagesc(pP'); title('Predicted State')
-p2=subplot(4,1,2); imagesc(pU'); title('Updated State')
-p3=subplot(4,1,3); imagesc(pS'); title('Smoothed State')
-p4=subplot(4,1,4); hold on; plot((100*Lfz/BW),'DisplayName','% BW on left'); 
-plot(100-(100*Rfz/BW),'DisplayName','100-%BW on right');
-plot(((MAPstate-1)*100/99),'DisplayName','MAP estimate'); 
-linkaxes([p1 p2 p3 p4],'xy'); axis([1 size(Lfz,1) 0 100]); legend
+% figure; 
+% p1=subplot(4,1,1); imagesc(pP'); title('Predicted State')
+% p2=subplot(4,1,2); imagesc(pU'); title('Updated State')
+% p3=subplot(4,1,3); imagesc(pS'); title('Smoothed State')
+% p4=subplot(4,1,4); hold on; plot((100*Lfz/BW),'DisplayName','% BW on left'); 
+% plot(100-(100*Rfz/BW),'DisplayName','100-%BW on right');
+% plot(((MAPstate-1)*100/99),'DisplayName','MAP estimate'); 
+% linkaxes([p1 p2 p3 p4],'xy'); axis([1 size(Lfz,1) 0 100]); legend
 
 %% Visualize aligned results
 figure; 
@@ -34,7 +39,7 @@ tl={'Predicted','Updated','Smoothed'};
 ev=events06; %Use classic-detection events
 ev=eventsK; %Use kalma-filter based events -> If this is better, the forces should look better aligned to these events, both in mean/median and dispersion values
 for i=1:4 %Align to all 4 events
-    window=[-30:30];
+    window=[-200:200];
     aux=find(ev(:,i))+window;
     for j=1:3
     b=squeeze(mean(reshape(p{j}(aux(:),:),size(aux,1),size(aux,2),size(p{j},2)),1));
@@ -53,8 +58,13 @@ for i=1:4 %Align to all 4 events
     %plot(100*median(Lfz(aux),1)/BW,'DisplayName','median % BW on left'); 
     %plot(100-(100*median(Rfz(aux),1)/BW),'DisplayName','median 100-%BW on right');
     %plot(((median(MAPstate(aux),1)-1)*100/99),'DisplayName','median MAP estimate');
+    if mod(i,2)==1
     plot(100*(Lfz(aux))'/BW,'DisplayName','% BW on left','Color',l1.Color); 
+    axis([100 300 0 15])
+    else
     plot(100-(100*(Rfz(aux))'/BW),'DisplayName','100-%BW on right','Color',l2.Color);
+    axis([100 300 85 100])
+    end
     plot((((MAPstate(aux))-1)'*100/99),'DisplayName','MAP estimate','Color',l3.Color); 
     %TODO add some sense of dispersion to the plots
     set(p1,'XTick',[window(1),0:100:window(end)]-window(1)+1,'XTickLabel',{num2str(window(1)),Elabels{i}(end-2:end),'+100','+200'})
@@ -69,4 +79,29 @@ end
 %plot(((MAPstate-1)*100/99),'DisplayName','MAP estimate'); 
 %linkaxes([p1 p2 p3 p4],'xy'); axis([1 size(Lfz,1) 0 100]); 
 
-%% To DO: visualize actual forces to distribution of estimated forces.-> Is the actual force close to the MAP?
+%%  Visualize difference in forces during 'stance'
+ev=full(events06);
+stanceL{1}=1+cumsum(ev(:,1))-cumsum(ev(:,3));
+stanceR{1}=1+cumsum(ev(:,2))-cumsum(ev(:,4));
+DS_LR{1}=1+cumsum(ev(:,1))-cumsum(ev(:,4));
+DS_RL{1}=1+cumsum(ev(:,2))-cumsum(ev(:,3));
+ev=full(eventsK);
+stanceL{2}=cumsum(ev(:,1))-cumsum(ev(:,3));
+stanceL{2}=stanceL{2}-median(stanceL{2})+1;
+stanceR{2}=cumsum(ev(:,2))-cumsum(ev(:,4));
+stanceR{2}=stanceR{2}-median(stanceR{2})+1;
+DS_LR{2}=1+cumsum(ev(:,1))-cumsum(ev(:,4));
+DS_RL{2}=1+cumsum(ev(:,2))-cumsum(ev(:,3));
+
+%Non-zero forces measured during alleged 'swing': (Type I error), sometimes happens
+sum(Lfz(stanceL{1}==0)~=0)
+sum(Lfz(stanceL{2}==0)~=0)
+sum(Rfz(stanceR{1}==0)~=0)
+sum(Rfz(stanceR{2}==0)~=0)
+
+%0-force samples during alleged 'stance': (Type II error), this should almost never happen
+sum(Lfz(stanceL{1}==1)==0)
+sum(Lfz(stanceL{2}==1)==0)
+sum(Rfz(stanceR{1}==1)==0)
+sum(Rfz(stanceR{2}==1)==0)
+
